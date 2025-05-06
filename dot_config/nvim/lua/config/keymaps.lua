f4e424b8851a6aa43f54050f179663114734b933
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-05-06 19:26:10 +0900

    Change nvim keyconfig

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -6,25 +6,24 @@
 vim.keymap.set("i", "jj", "<esc>", { noremap = true, silent = true })
 
 -- skkeleton
-vim.keymap.set({ "i", "c" }, "<C-j>", [[<Plug>(skkeleton-toggle)]], { noremap = false })
+vim.keymap.set({ "i", "c" }, "<C-o>", [[<Plug>(skkeleton-toggle)]], { noremap = false })
 --[[
  SKK keybindings
- (insert mode)CTRL-j : 全角ひらがな入力
+ (insert mode)CTRL-o : 全角ひらがな入力
  (Insert mode(SKK))q : 全角カタカナ<->全角ひらがな
  (Insert mode(SKK))CTRL-q : 半角カタカナ<->全角ひらがな
- (Insert mode(SKK))L : 全角英数入力(CTRL-j で戻る)
+ (Insert mode(SKK))L : 全角英数入力(CTRL-o で戻る)
  (Insert mode(SKK))l : 半角英数入力(insert mode)に戻る
  (Insert mode(SKK:SHIFTで入力開始))SPACE : 変換
- (Insert mode(SKK:SHIFTで入力開始))CTRL-j : 確定
+ (Insert mode(SKK:SHIFTで入力開始))CTRL-o : 確定
  (Insert mode(SKK:SHIFTで入力開始))CTRL-g : 入力キャンセル
  (Insert mode(SKK:SHIFTで入力開始))q : 全角ひらがなで入力していたら全角カタカナに変換する
  (Insert mode(SKK:変換中))x : 変換候補を戻る
  (Insert mode(SKK:変換中))CTRL-g : 変換中止
 --]]
 --
--- Esc キーでターミナル入力モードからノーマルモードに戻れる
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+-- jj キーでターミナル入力モードからノーマルモードに戻れる
+vim.keymap.set("t", "jj", [[<C-\><C-n>]])
 
 -- https://zenn.dev/skanehira/articles/2022-01-29-vim-cursor-jump
--- vim.keymap.set("n", "<Leader>j", "<Plug>(jumpcursor-jump)")
 vim.keymap.set("n", "t", "<Plug>(jumpcursor-jump)")
