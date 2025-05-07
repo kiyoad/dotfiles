e715f5c784659ea444c8bc84d413d7b65453de1b
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-05-08 06:49:33 +0900

    Add neovim tools

--- dot_config/nvim/lua/plugins/hlchunk.lua
+++ dot_config/nvim/lua/plugins/hlchunk.lua
@@ -3,7 +3,29 @@ return {
     "shellRaining/hlchunk.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require("hlchunk").setup({})
+      require("hlchunk").setup({
+        chunk = {
+          enable = false,
+        },
+        indent = {
+          enable = false,
+        },
+        line_num = {
+          enable = true,
+        },
+        blank = {
+          enable = true,
+          chars = {
+            " ",
+          },
+          style = {
+            { bg = "#434437" },
+            { bg = "#2f4440" },
+            { bg = "#433054" },
+            { bg = "#284251" },
+          },
+        },
+      })
     end,
   },
 }
