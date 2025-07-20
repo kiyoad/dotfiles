9f0532bca8860c83005183761f0a06a336fb910e
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-07-20 17:35:26 +0900

    Change nvim_context_vt config

--- dot_config/nvim/lua/plugins/nvim_context_vt.lua
+++ dot_config/nvim/lua/plugins/nvim_context_vt.lua
@@ -2,5 +2,11 @@ return {
   {
     -- Shows virtual text of the current context after functions, methods, statements, etc.
     "andersevenrud/nvim_context_vt",
+    event = "VeryLazy",
+    config = function()
+      require("nvim_context_vt").setup({
+        disable_virtual_lines = true,
+      })
+    end,
   },
 }
